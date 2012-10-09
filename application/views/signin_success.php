f9e13f34357d06d696d7b0aa6dddaad203c5d5e9
John Doyle <john.c.doyle@gmail.com>
2012-10-09 01:23:17 -0400

    Fix spelling

--- application/views/signin_success.php
+++ application/views/signin_success.php
@@ -4,7 +4,7 @@ $attributes = array('class' => '', 'id' => '');
 
 ?>
 
-<h1>Signed InQ</h1>
+<h1>Signed In</h1>
 
 <p>Congrats <?php echo $this->session->userdata('user_name'); ?> for signing in.</p>
 
