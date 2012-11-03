2eb2d649e792009b36ac073e755604ecf4160add
John Doyle <john.c.doyle@gmail.com>
2012-11-03 15:22:30 -0400

    fix karma on two other pages referencing it.

--- application/views/public_main.php
+++ application/views/public_main.php
@@ -104,11 +104,11 @@ foreach ($posts as $msg) :
                                 <a href="/posts/edit/<?php echo $msg->id; ?>" class="btn btn-mini btn-warning"><i class="icon-edit"></i></a>
                                 <a href="/posts/delete/<?php echo $msg->id; ?>" class="btn btn-mini btn-warning"><i class="icon-trash"></i></a>
 				<?php } if (($this->session->userdata('user_id')) && $this->session->userdata('user_id') != $msg->user_id) { ?>
-                                <a href="/karma/up/<?php echo $msg->id; ?>" class="btn btn-mini btn-success"><i class="icon-thumbs-up"><?php if ($msg->karma_up > 0) { echo $msg->karma_up;} ?></i></a>
-                                <a href="/karma/down/<?php echo $msg->id; ?>" class="btn btn-mini btn-danger"><i class="icon-thumbs-down"><?php if ($msg->karma_down > 0) { echo $msg->karma_down;} ?></i></a>
+                                <a href="/karma/up/<?php echo $msg->id; ?>" class="btn btn-mini btn-success"><i class="icon-thumbs-up"></i><?php if ($msg->karma_up > 0) { echo $msg->karma_up;} ?></a>
+                                <a href="/karma/down/<?php echo $msg->id; ?>" class="btn btn-mini btn-danger"><i class="icon-thumbs-down"></i><?php if ($msg->karma_down > 0) { echo $msg->karma_down;} ?></a>
 				<?php } else { ?>
-                                <button class="btn btn-mini btn-success"><i class="icon-thumbs-up"><?php if ($msg->karma_up > 0) { echo $msg->karma_up;} ?></i></button>
-                                <button class="btn btn-mini btn-danger"><i class="icon-thumbs-down"><?php if ($msg->karma_down > 0) { echo $msg->karma_down;} ?></i></button>
+                                <button class="btn btn-mini btn-success"><i class="icon-thumbs-up"></i><?php if ($msg->karma_up > 0) { echo $msg->karma_up;} ?></button>
+                                <button class="btn btn-mini btn-danger"><i class="icon-thumbs-down"></i><?php if ($msg->karma_down > 0) { echo $msg->karma_down;} ?></button>
 				<?php } ?>
                         </span>
 		</div>
